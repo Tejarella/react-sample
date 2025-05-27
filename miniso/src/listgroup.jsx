ee4acbe5b030136a1e3af95645355d76c9493136
Tejaswi_Arella <tejaswiarella@gmail.com>
2025-05-27 09:37:51 +0530

    Updated App.jsx and other files

--- miniso/src/listgroup.jsx
+++ miniso/src/listgroup.jsx
@@ -1,19 +1,35 @@
-function ListGroup()
-{
-    const items = ["New York","L A","London","Tokyo","Hyderabad"]
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-    if(items.length === 0)
-        return <p>No items found</p>;
-    
-    return(
-        <>
-        <h1>List of Places</h1>
-        <ul className="list-group">
-            {items.map((item) =>(
-                <li key={item}>{item}</li>
-            ))}
-        </ul>
-        </>
-    )
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
-export default ListGroup;
+
+export default App
